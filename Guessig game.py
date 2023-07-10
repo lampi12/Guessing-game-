d011507dd932738c6125070bfeb5c6ba3ecc1fb6
lampi12 <61427233+lampi12@users.noreply.github.com>
2023-07-10 17:20:37 -0400

    Update Guessig game.py

--- Guessig game.py
+++ Guessig game.py
@@ -1,8 +1,4 @@
 # Wind-chill Temperature
-# U64412214
-# Name: Hyelampa Thlala Kolo
-'''This program is intended to generate a random number and have the user guess that number correctly to win. The program also counts
-   the amount of times the user has guesses. The user only have 10 guess then the game is over'''
 import random
 
 num = random.randint(1, 101)
